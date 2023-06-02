7dcae0451138a021ed23cd4bb675dd5a9989f72f
matthew <matthew.almeida@sbcglobal.net>
2023-06-02 18:25:51 -0400

    product/product images models

---
+++ app/models/product.py
@@ -0,0 +1,31 @@
+from .db import db, add_prefix_for_prod
+
+class Product(db.Model):
+    __tablename__ = "products"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    available = db.Column(db.Integer, nullable=False)
+    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updatedAt = db.Column(db.DateTime, nullable=False, default=datetime.now())
+
+    images = db.relationship("ProductImage", back_populates="product")
+    user = db.relationship("User", back_populates="products")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "userId": self.userId,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "available": self.available,
+            "createdAt": self.createdAt,
+            "updatedAt": self.updatedAt
+       }
