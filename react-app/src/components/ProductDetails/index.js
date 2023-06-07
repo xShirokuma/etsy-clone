d7bab734345b3e6c94cfdbc35392e700b4010048
matthew <matthew.almeida@sbcglobal.net>
2023-06-07 11:40:30 -0400

    review image fix

--- react-app/src/components/ProductDetails/index.js
+++ react-app/src/components/ProductDetails/index.js
@@ -3,74 +3,75 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchProducts } from "../../store/products";
 import ImageCarousel from "./Carousel";
-import { useHistory , Link} from "react-router-dom";
-import OpenModalButton from '../../components/OpenModalButton'
-import DeleteReview from '../../components/DeleteProduct'
+import { useHistory, Link } from "react-router-dom";
+import OpenModalButton from "../../components/OpenModalButton";
+import DeleteReview from "../../components/DeleteProduct";
 import PostReviewModal from "../../pages/PostReviewPage";
 import "./productdetails.css";
-
 
 const ProductDetails = () => {
   const dispatch = useDispatch();
   const history = useHistory();
-  const {productId} = useParams()
-  const product = (useSelector((state) => state?.products[productId]))
+  const { productId } = useParams();
+  const product = useSelector((state) => state?.products[productId]);
   // console.log("product",product)
   // console.log("id",productId)
   // console.log("reviews",product.reviews)
 
   useEffect(() => {
-      dispatch(fetchProducts());
-    }, [dispatch]);
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
   return (
     <div className="product-single">
-        <h1>Product Details</h1>
-        <div>{product?.name}</div>
-        {/* <img src={product?.previewImage} /> */}
-        <div>
-          <ImageCarousel />
-        </div>
-        <div>{product?.description}</div>
-        <div>{product?.price}</div>
-        <div>{product?.available}</div>
-        {
-          product?.reviews?.map(r=> {
-            return (
-              <div>
+      <h1>Product Details</h1>
+      <div>{product?.name}</div>
+      {/* <img src={product?.previewImage} /> */}
+      <div>
+        <ImageCarousel />
+      </div>
+      <div>{product?.description}</div>
+      <div>{product?.price}</div>
+      <div>{product?.available}</div>
+      {product?.reviews?.map((r) => {
+        return (
+          <div>
             <div>{r?.review}</div>
             <div>{r?.stars}</div>
-            {/* <div>{r.images[0]}</div> */}
-            {/* {
-            r.images?.map(i =>{
-              return(
-                <div>
-                <h1>Hello</h1>
-                <img src={i.imageUrl}></img>
+            {product?.reviews?.map((r) => {
+              return (
+                <div key={r.id}>
+                  <div>{r.review}</div>
+                  <div>{r.stars}</div>
+                  {r.images.map((i) => {
+                    return (
+                      <div key={i.id}>
+                        <h1>Hello</h1>
+                        <img src={i.imageUrl} alt="Review Image" />
+                      </div>
+                    );
+                  })}
                 </div>
-              )
-            })
-            } */}
-           
-            </div>
-            )
-          })
-            
-            
-        }
+              );
+            })}
+          </div>
+        );
+      })}
 
-        <div>
-            {/* <Link product={product} onClick={(e)=>history.push(`/products/${productId}/review`)} >Post a review</Link> */}
-            <OpenModalButton 
-                             buttonText="Create" modalComponent={<PostReviewModal productId={productId} />} 
-                             /> 
-            {/* <button onClick={(e)=>history.push(`/products/${product.id}/delete`)} product={product}>Delete a review</button> */}
-            <OpenModalButton 
-                             buttonText="Delete" modalComponent={<DeleteReview product={product} />} 
-                             /> 
-        </div>
+      <div>
+        {/* <Link product={product} onClick={(e)=>history.push(`/products/${productId}/review`)} >Post a review</Link> */}
+        <OpenModalButton
+          buttonText="Create"
+          modalComponent={<PostReviewModal productId={productId} />}
+        />
+        {/* <button onClick={(e)=>history.push(`/products/${product.id}/delete`)} product={product}>Delete a review</button> */}
+        <OpenModalButton
+          buttonText="Delete"
+          modalComponent={<DeleteReview product={product} />}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetails;
