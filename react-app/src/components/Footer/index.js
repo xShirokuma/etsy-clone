878e063deeae8064198551bda838aee840612ce7
Matthew <matthew.almeida@sbcglobal.net>
2023-06-09 18:18:56 -0400

    added footer

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
+
+function Footer({ isLoaded }) {
+  return (
+    <div className="footer">
+      <div className="about">
+        <div className="about-grid-one">
+          <div>
+            <p>Matthew Almeida</p>
+            <a href="https://github.com/ImTheChosen0ne">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/matthew-almeida-103425183/">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+          <div>
+            <p>Sheena Gupta</p>
+            <a href="https://github.com/coderaries12">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/sheena1204/">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+        <div className="about-grid-two">
+          <div>
+            <p>Jen Liu</p>
+            <a href="https://github.com/JENLIU2023">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/your-linkedin-profile">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+          <div>
+            <p>Chris Thibault</p>
+            <a href="https://github.com/xShirokuma">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h3>
+            <i class="fa-regular fa-copyright"></i>2023 Shopsy inspired by Esty
+          </h3>
+        </div>
+        <div>
+          <div>Made with:</div>
+          <div className="madeIcons">
+            <i class="fa-brands fa-react"></i>
+            <i class="fa-brands fa-html5"></i>
+            <i class="fa-brands fa-css3-alt"></i>
+            <i class="fa-brands fa-square-js"></i>
+            <i class="fa-brands fa-python"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
