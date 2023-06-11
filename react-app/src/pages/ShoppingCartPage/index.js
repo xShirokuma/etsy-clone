fc4707e913d3b182a3398526a28692cb050fee8b
Jen Liu <jingyuliu01@gmail.com>
2023-06-11 16:01:12 -0700

    css

--- react-app/src/pages/ShoppingCartPage/index.js
+++ react-app/src/pages/ShoppingCartPage/index.js
@@ -32,41 +32,42 @@ const ShoppingCartPage = () => {
 
     return (
         <div className="bodyContainer">
+          <div className="cartpage">
             <h1>{title}</h1>
-            <div className="cartpage">
-            {sessionUser?.cart_session?.cart.map((ele) => (
-                  <div key={ele.product.id}>
-                    <div>
-                      <NavLink to={`/products/${ele.product.id}`}>
+            <div className="product-in-cartpage">
+              {sessionUser?.cart_session?.cart.map((ele) => (
+                <div key={ele.product.id} className="item-in-shop">
+                    <NavLink to={`/products/${ele.product.id}`}>
                         <img
                           className="product-image"
                           src={ele.product.previewImage}
                           alt="products"
                         />
                       {/* <div className="price">$ {ele.product.price.toFixed(2)}</div> */}
-                      </NavLink>
-                      <div>$ {ele.product.price.toFixed(2)}</div>
-                      <div>quantity: {ele.quantity}</div>
-                      
-                    <OpenModalButton
-                    buttonText="Delete Product"
+                    </NavLink>
+                    <div className="item-in-shop-info">
+                      <div>Product: {ele.product.name}</div>
+                      <div>Price: $ {ele.product.price.toFixed(2)}</div>
+                      <div>Quantity: {ele.quantity}</div>
+                      <div>Total: $ {ele.product.price.toFixed(2) * ele.quantity}</div>
+                    </div>
+                    <div className="delete-item-in-cart-btn">
+                      <OpenModalButton
+                    buttonText="Delete Item"
                     modalComponent={
                       <DeleteShoppingCart
                         cartId={ele.id}
                         productId={ele.product.id}
                         sessionuserId={sessionUser.id}
-                      
                       />
                     }
                     />
-                      {/* <FavoriteIcon
-                        sessionUser={sessionUser}
-                        product={product}
-                      /> */}
                     </div>
-                  </div>
+                    </div>
                 ))}
             </div>
+          </div>
+            
             <div className="empty-cart">
               <h2 className={sessionUser?.cart_session?.cart.length >= 1? "hidden":""}>
                 Your cart is empty
@@ -78,7 +79,7 @@ const ShoppingCartPage = () => {
               </div >
             </div>
             <button onClick={checkout} 
-              className={sessionUser?.cart_session?.cart.length >= 1? "":"hidden"}>
+              className={sessionUser?.cart_session?.cart.length >= 1? "checkout-btn":"checkout-btn hidden"}>
                 Proceed to checkout
             </button>
         </div> 
